9ac7de6cd0f02e858780b7dade47188d7ca6a338
Hannah <hannah.bohlen.2021@student.lhschools.org>
2019-01-25 08:53:56 -0500

    Added first 2 numbers and Words

--- src/MathTester.java
+++ src/MathTester.java
@@ -1,4 +1,20 @@
 public class MathTester {
-    public static void main(String[] args){}
-    
+    public static void main(String[] args) {
+
+        int firstNumber;
+        int secondNumber;
+        String firstWord;
+        String secondWord;
+
+
+        firstNumber = 5;
+        secondNumber = 10;
+        firstWord= "dog";
+        secondWord = "house";
+
+
+        System.out.println(firstWord+firstNumber);
+
+    }
 }
+
